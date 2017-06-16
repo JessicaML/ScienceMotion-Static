526af7938f1c075e848accb525649b1c85d10df2
Jessica Leach <jessicamaryleach@gmail.com>
2017-06-16 10:19:42 +0100

    skroller mobile id added and start acdc longread

---
+++ ACDC/jquery-embedagram.pack.js
@@ -0,0 +1,8 @@
+/*
+ * embedagram - embed your instagram photos
+ *   http://embedagram.com/plugin.php
+ *
+ * Copyright (c) 2011 Matthew Hokanson (http://h0ke.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(0($){$.q.h({7:0(d){1 e={8:-v,4:u,2:\'n\',3:\'m\',5:l,f:0(){6 j},};1 d=$.h(e,d);6 g.k(0(){1 o=d;1 b=$(g);1 c="p://7.i/e/r/"+o.8+"/?s=?";c+="&4="+o.4+"&2="+o.2;c+="&5="+o.5+"&3="+o.3;$.t(c,0(a){b.9(a.9);o.f.w(b)});6 b})}})})(x);',34,34,'function|var|wrap_tag|link_type|thumb_width|limit|return|embedagram|instagram_id|html||||||success|this|extend|com|true|each|20|web|li||http|fn|plugin|callback|getJSON|100|999|apply|jQuery'.split('|'),0,{}))
