48664bbb179fbd48d7e397d857a6f7fad26daec3
utsha1510 <utsha.sinha1510@gmail.com>
2018-02-28 00:57:20 +0530

    initial commit

---
+++ comets_and_asteroids/movement.js
@@ -0,0 +1,19 @@
+function myMove() {
+    var img = new Image();
+  var elem = document.getElementById("animate"); 
+  img.onload = function() {
+  div.appendChild(img);
+};
+img.src = "comets and asteroids/images/images.png";
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.right = pos + 'px'; 
+    }
+  }
+}
