0c54e695f1f022b63f07b71b23285bb792e92e50
tanisha007h <36270352+tanisha007h@users.noreply.github.com>
2018-03-23 23:40:09 +0530

    New Version with changes included

--- bacteria/animate.js
+++ bacteria/animate.js
@@ -1,68 +1,82 @@
 $(document).ready(function(){
     $("#cap").mouseenter(function(){
        $("#capsule").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#cap").mouseleave(function(){
        $("#capsule").css('display','none');
+        $('#base_').css('display','block');
     });
   $("#celm").mouseenter(function(){
        $("#cellm").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#celm").mouseleave(function(){
        $("#cellm").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#celw").mouseenter(function(){
        $("#cellw").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#celw").mouseleave(function(){
        $("#cellw").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#cyt").mouseenter(function(){
        $("#cyto").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#cyt").mouseleave(function(){
        $("#cyto").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#fla").mouseenter(function(){
        $("#flage").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#fla").mouseleave(function(){
        $("#flage").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#incl").mouseenter(function(){
        $("#inc").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#incl").mouseleave(function(){
        $("#inc").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#nucl").mouseenter(function(){
        $("#nuc").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#nucl").mouseleave(function(){
        $("#nuc").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#pil").mouseenter(function(){
        $("#pili").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#pil").mouseleave(function(){
        $("#pili").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#pla").mouseenter(function(){
        $("#plasm").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#pla").mouseleave(function(){
        $("#plasm").css('display','none');
+       $('#base_').css('display','block');
     });
   $("#rib").mouseenter(function(){
        $("#ribo").css('display','block');
+       $('#base_').css('display','none');
     });
   $("#rib").mouseleave(function(){
        $("#ribo").css('display','none');
-    });
-  $("#bacell").mouseenter(function(){
-       $("#bacell").attr({"src":"bacstruc.jpg","width":"900px","height":"700px"});
-    });
-  $("#bacell").mouseleave(function(){
-       $("#bacell").attr({"src":"baccell.gif"});
+       $('#base_').css('display','block');
     });
 });
