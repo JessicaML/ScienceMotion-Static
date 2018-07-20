1f4ffbb594b7bfd9b493fb04788a8fb18938cddf
Jessica Leach <jessica@info.nl>
2018-07-20 09:58:37 +0200

    smooth scroll added to see more button

--- js/scrollnav.js
+++ js/scrollnav.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
   function scrollNav() {
-    $('#bs-example-navbar-collapse-1 a').click(function(){
-      $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 160
-      }, 800);
+    $("#smoothscroll a").click(function() {
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($(this).attr("href")).offset().top - 160
+          },
+          800
+        );
     });
 
-    $('.scrollTop a').scrollTop();
+    $(".scrollTop a").scrollTop();
   }
 
   scrollNav();
-
 });
