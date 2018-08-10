cc12d8ceb1c4c00f0dd13d53f9d919a153492663
nirmala <nirmala@surf.net.np>
2018-08-10 15:34:18 +0200

    Add animations to the lesson cell, tissue & organ

--- Biology/Levels/assets/main.js
+++ Biology/Levels/assets/main.js
@@ -1,7 +1,37 @@
-$(document).ready(function(){
-    $("#click").click(function(){ 
-        $(".cell").css({ transform: 'scale(.1)' });
-        $(".cell").clone().appendTo("#cell-clone").animate({left: "200px"});
+    function makeCell() {
+        var divsize = 50;
+        var posx = (Math.random() * ($(".tissue-container").width() - divsize)).toFixed();
+        var posy = (Math.random() * ($(".tissue-container").height() - divsize)).toFixed();
+        $newCell = $("<div class='cell-clone'></div>").clone().css({
+            'left': posx + 'px',
+            'top': posy + 'px',
+            
+        });
+ 
+        $newCell.appendTo('.tissue-container');
+    }
+
+
+    function multiplyCell(){
+        for(var i = 0; i < 100; i++){
+            setTimeout(function timer(){
+                makeCell();
+            }, i * 65); 
+        }           
+    }
+
+
+setTimeout( multiplyCell, 11000 );
+
+$(function(){
+        $('.organ').prepend('<img src="assets/images/heartfinal_animated.svg" />').hide().delay(22000).queue(function(n) {
+            $(this).show(); n();
+        })
     });
+
+$(function(){
+    $('.organ-system').prepend('<img src="assets/images/organsystemfinalslow_animated.svg" />').hide().delay(26000).queue(function(n) {
+        $(this).show(); n();
+    })
 });
 
